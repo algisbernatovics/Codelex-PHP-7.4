ecc47ec827e2b7f6511c6852619ac9a1ba1e00dc
algisbernatovics <miskastespams@gmai.com>
2023-04-13 22:51:42 +0300

    Composer GuzzleHttp added

--- api/weather-report/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function digits(string $value): int
-{
-    return (int)preg_replace('/[^0-9]/', "", $value, -1);
-}
-//Function For weather-report Regex Digits only (Without Negative Digits);
--- api/weather-report/index.php
+++ api/weather-report/index.php
@@ -1,4 +1,8 @@
-<?php include 'functions.php';
+<?php
+
+use GuzzleHttp\Psr7\Request;
+
+require 'vendor/autoload.php';
 
 $cities = ['Riga', 'Jelgava', 'Dobele', 'Berlin'];
 
@@ -13,23 +17,30 @@ foreach ($cities as $key => $city) {
 echo PHP_EOL;
 echo PHP_EOL;
 
-$choice = digits(readline('Enter favourite City By number:'));
+$choice = (int)(readline('Enter favourite City By number:'));
 
 while (!$choice == 0 && $choice >= count($cities)) {
-    $choice = digits(readline('Enter correct city by number:'));
+    $choice = (int)(readline('Enter correct city by number:'));
 };
 
 $city = $cities[$choice];
 $apikey = '9b023939292af51079afe14264490ab6';
 
-$urlContents = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apikey");
-$weatherArray = json_decode($urlContents, true);
+$client = new GuzzleHttp\Client(['base_uri' => 'https://api.openweathermap.org/']);
+$request = new Request('GET', "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apikey");
+$promise = $client->sendAsync($request)->then(function ($response) {
 
-echo PHP_EOL;
-echo 'The weather in ' . $weatherArray['name'] . ' is currently ' . $weatherArray['weather'][0]['description'] . '·';
-echo PHP_EOL;
-echo 'The temperature in ' . $weatherArray['name'] . ' is ' . intval($weatherArray['main']['temp'] - 273) . 'C';
-echo PHP_EOL;
-echo 'and the wind speed is ' . $weatherArray['wind']['speed'] . " m/s.";
-echo PHP_EOL;
-echo PHP_EOL;
+    $weatherArray = (json_decode($response->getBody(), true));
+
+    echo PHP_EOL;
+    echo 'The weather in ' . $weatherArray['name'] . ' is currently ' . $weatherArray['weather'][0]['description'] . '·';
+    echo PHP_EOL;
+    echo 'The temperature in ' . $weatherArray['name'] . ' is ' . intval($weatherArray['main']['temp'] - 273) . 'C';
+    echo PHP_EOL;
+    echo 'and the wind speed is ' . $weatherArray['wind']['speed'] . " m/s.";
+    echo PHP_EOL;
+    
+});
+$promise->wait();
+
+
